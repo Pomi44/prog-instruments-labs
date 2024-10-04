527be3a4c17e6a3f71268c4b756e45daeaede770
Pomi44 <ksushak1803@gmail.com>
2024-10-04 15:07:47 +0400

    удален весь закоментированный код

--- lab_1/PhyRe.py
+++ lab_1/PhyRe.py
@@ -1,57 +1,7 @@
-#!/usr/bin/python
-
-"""
-CODENAME:     PhyRe
-DESCRIPTION:  
-
-Copyright (c) 2009 Ronald R. Ferrucci, Federico Plazzi, and Marco Passamonti..
-
-
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-"""
-
 import sys
 
 samplefile = sys.argv[1];      del sys.argv[1]
 popfile = sys.argv[1];      del sys.argv[1]
-#outfile= sys.argv[1];      del sys.argv[1]
-
-#outfile = samplefile
-#output = open(outfile, 'w')
-
-#efile = open('error.log','w')
-#sys.stderr = efile
-
-#output = open('output','w')
-#out.write(allelesfile)
-#out.close()
-###-----------------options-------------------------###
-
-"""p = permutations for confidence intervals, d1 and d2 are range for number of 
-species for funnel plot. parameter: m = AvTD, v = VarTD, e = euler, b = AvTD and VarTd. 
-ci = confidence intervals b = batch file. l = user-defined path lengths
-"""
 
 p = 1000; d1 = 10; d2 = 70; ci = 'y'; b = 'n'; l = 'n'
 batch = b; pathlengths = l; missing = 'n'
@@ -121,32 +71,7 @@ Index = {}; Taxon = {}; coef = {}; Taxon = {}; taxon = []
 
 pathLengths= {}
 
-for i in open(samplefile):
-    """
-    if match('Taxon:', i):
-        x = i.split()
-        x.remove('Taxon:')
-        #x = [string.lower() for string in x]  
-
-        for i in x:
-            taxon.append(i)
-            j = x.index(i)
-            Index[i] = j + 1
-        continue
-
-    elif match('Coefficients:', i):
-        x = i.split()
-        x.remove('Coefficients:')
-        x = map(eval, x)
-        
-        for t in taxon:
-            i = taxon.index(t)
-            coef[t] = sum(x[i:])
-            pathLengths[t] = x[i]
-
-        continue
-    """
-    
+for i in open(samplefile):  
     if batch == 'y':
         j = i.strip()
         Files.append(j)
@@ -182,9 +107,6 @@ for i in open(popfile):
     i.strip()
     x = i.split()    
 
-    #if match('Taxon:', i): continue
-    #if match('Coefficients:', i): continue
-
     species = x[0]; population[species] = {}
     
     if species in sample.keys():
@@ -198,7 +120,6 @@ for i in open(popfile):
         mtax = ''
         for t in taxon:
             if x[Index[t]] == '/':
-                #sample[species][t] = sample[species][t]
                 sample[species][t] = mtax
             else:
                 sample[species][t] = x[Index[t]]
@@ -208,15 +129,8 @@ for i in open(popfile):
               
     else:
         for t in taxon:
-            #y = Taxon[t]
             sample[species][t] = x[Index[t]]
 	    population[species][t] = sample[species][t]
-	    
-    #for t in taxon:
-        #y = Taxon[t]
-    #    population[species][t] = x[Index[t]]
-
-
 if len(duplicates) > 0:
     print "Population master list contains duplicates:"
     for i in duplicates: print i,'\n'
@@ -242,8 +156,6 @@ def PathLength(population):
     n = [float(len(Taxon[t])) for t in taxon]
     
     n.insert(0,1.0)
-        
-    #s = 100/float(N)
     raw = []
     for i in range((len(n)-1)):
         j = i + 1
@@ -272,16 +184,11 @@ if pathlengths == 'y':
     XXX, popN, YYY = PathLength(population)
     del XXX, YYY
 
-#N = len(sample.keys())
+
 def ATDmean(data,sample):
-    #[sample = data.keys()
     N = len(sample)
     
     Taxon = {}; taxonN = {}; AvTD = 0; n = 0
-    #Taxon are counts of taxa at each level, taxonN are numbers of pairwise differences
-    #at each level, with n being the accumlation of pairwise differences at that level. the difference
-    #between n and TaxonN is the number of species that are in different taxa in that level
-    #but not in upper levels
 
     for t in taxon:
         Taxon[t] = {}
@@ -294,16 +201,12 @@ def ATDmean(data,sample):
         n = taxonN[t] - n
         AvTD = AvTD + (n * coef[t]) 
         n = taxonN[t]
-
-    #print sample
     AvTD /= (N * (N - 1))
 
     return AvTD,taxonN, Taxon
 
 def ATDvariance(taxonN, sample, atd):
     vtd = []
-    
-    #N = sum(taxon)
 
     vtd = 0; N = 0; n = 0
 
@@ -316,8 +219,6 @@ def ATDvariance(taxonN, sample, atd):
     n = N * (N - 1)
 
     vtd = (vtd - ((atd*n)**2)/n)/n
-
-    #vtd = (sum([tax1,tax2,tax3,tax4]) - (((atd * n)**2)/n))/n
 
     return vtd
     
@@ -334,12 +235,6 @@ def euler(data, atd, TaxonN):
 
     TDmin /= (n * (n-1))
 
-    #Taxon = {}
-
-    #tax = []
-
-    #taxon.append('sample')
-    #Taxon['sample'] = sample
     taxon.reverse()
     TaxMax = {}
 
@@ -371,8 +266,6 @@ def euler(data, atd, TaxonN):
         n = taxonN[t] - n
         TDmax += n * coef[t]
         n = taxonN[t]
-        #for i in TaxMax[t]:
-        #    print t, len(i)
 
     TDmax /= (N * (N-1))
 
@@ -393,7 +286,6 @@ def Sample(samplefile):
         x = i.split()
 
         species = x[0]
-        #sample[species] = {}
 
         sample[species] = population[species]
 
@@ -424,15 +316,7 @@ for f in Files:
 
 N = len(sample.keys())
 
-def printResults():
-    #if parameter == 'm':
-    #if parameter == 'm':
-    #    print "parameter is Average Taxonomic Distinctness\n"
-    #elif parameter == 'v':
-    #    print "parameter is Variation in Taxonomic Distinctness\n"
-    #elif parameter == 'e':
-    #    print "parameter is Euler's Index of Imbalance\n"
-            
+def printResults():    
     print "Number of taxa and path lengths for each taxonomic level:"
 
     for t in taxon:
@@ -469,11 +353,6 @@ at each level excluding comparisons that differ at upper levels"""
 printResults()
 print "---------------------------------------------------"
 
-#sys.stdout = saveout
-
-#sys.stdout=sys.__stdout__
-
-
 sys.stdout = saveout
     
 sys.stdout=sys.__stdout__
@@ -491,17 +370,6 @@ limits are lower 95% limit for AvTD and upper 95% limit for VarTD
 """
     print "Number of permutations for confidence limits =", p, '\n'
 
-    #if paramter == 'm':
-    #    print "Confidence limits for Average Taxonomic Distinctiveness are in file ", output
-    #if paramter == 'v':
-    #    print "Confidence limits for Variation in Taxonomic Distinctiveness are in file ", output
-   
-    
-    #o = open('sample2.txt','w')
-    
-    #saveout = sys.stdout
-    #sys.stdout = open(output, 'w')
-    
     ciarray = []; x = [];carray = []
     def Funnel(p,d1,d2):
         from random import sample
@@ -511,10 +379,6 @@ limits are lower 95% limit for AvTD and upper 95% limit for VarTD
 
         print "dimension AvTD05%   AvTDmean  AvTD95%   AvTDup    VarTDlow   VarTD05%   VarTDmean  VarTD95%"
         for d in range(d1, d2 + 1):
-        #for i in range(10):
-            #d = N
-            #if d != N: continue
-            #from math import max, min
             x.append(d)
             AvTDci = []; VarTDci = []
             for j in range(p):
@@ -532,59 +396,11 @@ limits are lower 95% limit for AvTD and upper 95% limit for VarTD
             dims.append(d)
             ciarray.append(AvTD[0])
             carray.append(AvTD[1])
-
-            #up.append(ci95[1])
-            #lo.append(ci95[0])
-            #means.append(mean)
             print '%i        %6.4f   %6.4f   %6.4f   %6.4f   %6.4f   %6.4f   %6.4f   %6.4f' \
                 %(d, AvTD[0], AvTD[1], AvTD[2], AvTD[3], VarTD[0], VarTD[1], VarTD[2], VarTD[3])
             
-            #if d == N:
-            #    Ie = (max(cache)-atd)/(max(cache)-min(cache))
-            #    print d, Ie, ci95, mean
-            
-        #return dims, up, lo, means
-            #print d,ci95
-    
     Funnel(p,d1,d2)
-    #dims, up, lo, means = Funnel(p,d1,d2)
 
     sys.stdout = saveout
         
     sys.stdout=sys.__stdout__
-    
-    #from QUASImage import *; from numpy import *
-    #ciarray = array(ciarray)
-    #from pgen import *
-
-    #ciarray += carray
-
-    #x *= 1
-    #charplot(x,ciarray)
-
-    #plot(ciarray)
-"""
-    from matplotlib.pylab import *
-    
-    if parameter == 'm':
-        param = 'Average Taxonomic Distinctiveness'
-    elif parameter == 'v':
-        param = 'Variation in Taxnomic Distinctiveness'
-    elif parameter == 'e':
-        param = 'Imbalance'
-    
-    #N = len(sample)    
-    #print N, atd
-    #figure(1)
-    plot(dims,up,dims, lo, dims, means)
-    title('ATD',fontstyle='italic')
-    xlabel('Number of Species')
-    ylabel(param,fontstyle='italic')
-    #savefig(figureOutput+".png")
-    
-    show()
-    
-    #sys.stdout = saveout
-    
-    #sys.stdout=sys.__stdout__
-"""
